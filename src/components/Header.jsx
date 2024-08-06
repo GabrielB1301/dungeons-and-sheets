3eaf824c3c8ba0a052c605cf797c4e4ce4574712
GabrielB1301 <gabrielcbeirigo@gmail.com>
2024-08-06 10:48:14 -0300

    style:header

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+export default function Header() {
+  return (
+    <div className="flex items-center gap-4">
+      <div>
+        <label htmlFor="picture" className="cursor-pointer">
+          <img
+            src="profile.png"
+            alt="profile-picture"
+            className="size-36 border"
+          />
+        </label>
+        <input
+          type="file"
+          name="profile-picture"
+          id="picture"
+          className="hidden"
+        />
+      </div>
+      <div className="flex flex-col items-start gap-1">
+        <label htmlFor="character-name" className="text-xl">
+          Character's Name
+        </label>
+        <input
+          type="text"
+          className="h-8 w-48 rounded p-1 text-neutral-950 outline-none"
+        />
+        <button>Randomize</button>
+      </div>
+    </div>
+  );
+}
